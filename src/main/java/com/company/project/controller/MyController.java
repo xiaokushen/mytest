691cabeef9a4f30b00f389a3b180523f6ba2021c
jiangshuxiong <jiangshuxiong@58.com>
2021-02-28 17:28:18 +0800

    my maven

---
+++ src/main/java/com/company/project/controller/MyController.java
@@ -0,0 +1,11 @@
+package com.company.project.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("")
+public class MyController {
+
+}
